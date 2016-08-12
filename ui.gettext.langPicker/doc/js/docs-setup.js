5993c2b1dd7064bc7b72ebc60a83f192b359364f
Gerasev Kirill <gerasev.kirill@gmail.com>
2016-08-12 16:38:36 +0300

    [+] docs: ui.gettext.langPicker

---
+++ ui.gettext.langPicker/doc/js/docs-setup.js
@@ -0,0 +1,52 @@
+NG_DOCS={
+  "sections": {
+    "api": "Angular API Reference"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "ui.gettext.langPicker",
+      "shortName": "ui.gettext.langPicker",
+      "type": "overview",
+      "moduleName": "ui.gettext.langPicker",
+      "shortDescription": "Language picker for https://angular-gettext.rocketeer.be",
+      "keywords": "$langpicker $stateprovider abstract add angular api app bootstrap code config cz de define detectlanguage deutsch directive en english gettext hello html https img lang langpicker language margin-bottom module myapp overview picker png prefix rocketeer router ru setlanguagelist src style template true ua ui ui-lang-picker url usual"
+    },
+    {
+      "section": "api",
+      "id": "ui.gettext.langPicker.$langPicker",
+      "shortName": "ui.gettext.langPicker.$langPicker",
+      "type": "service",
+      "moduleName": "ui.gettext.langPicker",
+      "shortDescription": "configuration service",
+      "keywords": "$langpicker api catalogue class code codes configuration currentlang cz de detectlanguage detector deutsch en english getcurrentlanguagename getter gettext https json label lang lang-codes lang-names langpicker language languagelist languages lazy-loading list navigator object org property reload remotecatalogurl rocketeer router ru service setcurrentlanguage setlanguagelist setlanguageremoteurl setter type-hint type-hint-object type-hint-string ua ui url user wikipedia window"
+    },
+    {
+      "section": "api",
+      "id": "ui.gettext.langPicker.directive:uiLangPicker",
+      "shortName": "uiLangPicker",
+      "type": "directive",
+      "moduleName": "ui.gettext.langPicker",
+      "shortDescription": "language picker directive for bootstrap framework(as button)",
+      "keywords": "api attribute bootstrap button directive disabled element expression framework gettext img jpg langpicker language margin-bottom ngdisabled output picker set src style truthy ui"
+    },
+    {
+      "section": "api",
+      "id": "ui.gettext.langPicker.directive:uiLangPickerForNavbar",
+      "shortName": "uiLangPickerForNavbar",
+      "type": "directive",
+      "moduleName": "ui.gettext.langPicker",
+      "shortDescription": "language picker directive for navbar from bootstrap framework",
+      "keywords": "api attribute bootstrap class directive disabled element expression framework gettext img jpg langpicker language margin-bottom nav navbar navbar-nav ngdisabled output picker set src style truthy ui ui-lang-picker-for-navbar"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api/ui.gettext.langPicker",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
